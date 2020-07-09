1059d77062f06407494645b7271750cc146b2899
Lucious Morrison <lmorrison1116@gmail.com>
2020-07-08 21:17:49 -0400

    removed built in enum classes from Card for usage with Sajid's and Briana's enum classes

--- src/main/java/com/codedifferently/casino/Card.java
+++ src/main/java/com/codedifferently/casino/Card.java
@@ -1,31 +1,5 @@
 package com.codedifferently.casino;
 
-enum Suit {
-    HEARTS("HEARTS"), SPADES("SPADES"), DIAMONDS("DIAMONDS"), CLUBS("CLUBS");
-    public String suit;
-
-    Suit(String suit) {
-        this.suit = suit;
-    }
-
-    public String getSuit() {
-        return this.suit;
-    }
-}
-
-enum Value {
-    ACEVALUEOF1(1), TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8
-    ), NINE(9), TEN(10),ACEVALUEOF11(11);
-    private int cardNum;
-
-    Value(int value) {
-        this.cardNum = value;
-    }
-
-    public int getValue(){
-        return this.cardNum;
-    }
-}
 
 public class Card {
     // enum types of suit and value
