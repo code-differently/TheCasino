a23445a22f239f8f576c970ac9a31aa6f1d9c330
beastlyCoder <imaaronacs@gmail.com>
2020-07-09 18:27:48 -0700

    try this

--- src/main/java/com/codedifferently/casino/WarGame.java
+++ src/main/java/com/codedifferently/casino/WarGame.java
@@ -20,9 +20,10 @@ public class WarGame extends Game
         super.playerTwo = new Player("Mandy");
 
 
-
         playersHand.put(super.playerOne, new Deck().generateDeck(5));
         playersHand.put(super.playerTwo, new Deck().generateDeck(5));
+
+
         int score1 = 0;
         int score2 = 0;
         for(Map.Entry<Player, ArrayList<Card>> playerCardEntry : playersHand.entrySet())
@@ -43,17 +44,19 @@ public class WarGame extends Game
                 {
                     System.out.println(p.getName() + " you've layed down a " + c.toString());
                     playerTwoValue = c.getValue().getValue();
-
                 }
+
                 if(playerOneValue > playerTwoValue)
                 {
                     System.out.println("Ha! Player One has won this round of war");
                     super.playerOne.setScore(score1++);
+                    System.out.println();
                 }
                 else if(playerTwoValue > playerOneValue)
                 {
                     System.out.println("Ha! Player Two has won this round of war");
                     super.playerTwo.setScore(score2++);
+                    System.out.println();
                 }
             }
         }
@@ -70,16 +73,16 @@ public class WarGame extends Game
     @Override
     public void tieMessage()
     {
-        System.out.println("Looks like there is a tie, we have chosen your tie breaking cards");
-        ArrayList<Card> playerOnesTie = new Deck().generateDeck(1);
-        System.out.println(super.playerOne.getName() + " your card's value is " + playerOnesTie.get(0).getValue() + " better hope "
-                + super.playerTwo.getName() + " doesn't have a higher card");
-
-        super.playerOne.setScore(playerOnesTie.get(0).getValue().getValue());
-        System.out.println(super.playerTwo.getName() + " we have selected your card, to keep it suspenseful, we wont tell you your card...");
-        ArrayList<Card> playerTwoTie = new Deck().generateDeck(1);
-        super.playerTwo.setScore(playerTwoTie.get(0).getValue().getValue());
-        checkStandings();
+//        System.out.println("Looks like there is a tie, we have chosen your tie breaking cards");
+//        ArrayList<Card> playerOnesTie = new Deck().generateDeck(1);
+//        System.out.println(super.playerOne.getName() + " your card's value is " + playerOnesTie.get(0).getValue() + " better hope "
+//                + super.playerTwo.getName() + " doesn't have a higher card");
+//
+//        super.playerOne.setScore(playerOnesTie.get(0).getValue().getValue());
+//        System.out.println(super.playerTwo.getName() + " we have selected your card, to keep it suspenseful, we wont tell you your card...");
+//        ArrayList<Card> playerTwoTie = new Deck().generateDeck(1);
+//        super.playerTwo.setScore(playerTwoTie.get(0).getValue().getValue());
+//        checkStandings();
 
     }
 
